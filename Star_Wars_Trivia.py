83ea2129e580c0fbf3be6d69bbe5a22437c282c6
Lia Pires <liasth.pires@gmail.com>
2022-07-09 18:24:38 -0300

    Creating deleting questions
    Creating deleting questions

--- Star_Wars_Trivia.py
+++ Star_Wars_Trivia.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template, abort, jsonify, request, redirect, url
 from model import db, save_db
 
 
+
 app = Flask(__name__)
 
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('home.html',
-                           questions=db)
+    return render_template('home.html', questions=db)
 
 
 @app.route('/questions/<int:index>')
 def questions_view(index):
+
     try:
         questions_db = db[index]
         return render_template('quiz.html', question=questions_db, index=index, max_index=len(db) - 1)
@@ -21,24 +23,12 @@ def questions_view(index):
         abort(404)
 
 
-@app.route('/api/question/')
-def api_question_list():
-    return jsonify(db)
-
-
-@app.route('/api/question/<int:index>')
-def api_question_detail(index):
-    try:
-        return db[index]
-
-    except IndexError:
-        abort(404)
-
-
-# tem que deixar explícito para aceitar POST
+# Adding a question
 @app.route('/add_new_question', methods=['GET', 'POST'])
 def add_question():
+
     if request.method == 'POST':
+
         question_dict = {
             "question": request.form['question'],
             "answer": request.form['answer']
@@ -53,6 +43,36 @@ def add_question():
         return render_template('add_question.html')
 
 
-if __name__ == '__main__':
+# Removing a Question
+@app.route('/remove_a_question/<int:index>', methods=['GET', 'POST'])
+def remove_question(index):
+    try:
+        if request.method == "POST":
+            del db[index]
+            save_db()
+            return redirect(url_for('home'))
+
+        else:
+            return render_template("remove_question.html", question=db[index])
+
+    except IndexError:
+        abort(404)
+
+
+# REST API for Multi-Page App
+@app.route('/api/question/')
+def api_question_list():
+    return jsonify(db)
+
+
+@app.route('/api/question/<int:index>')
+def api_question_detail(index):
+    try:
+        return db[index]
+
+    except IndexError:
+        abort(404)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-  
